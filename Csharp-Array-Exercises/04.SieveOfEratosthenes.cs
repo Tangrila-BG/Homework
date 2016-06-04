b1b9b6c3e00581b44ff3c9498f99eefbebbf46d9
Tangrila-BG <phatomiki@gmail.com>
2016-06-04 22:56:03 +0300

    Add files via upload

---
+++ Csharp-Array-Exercises/04.SieveOfEratosthenes.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SieveofEratosthenes
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            long range = long.Parse(Console.ReadLine());
+
+            bool[] primes = new bool[range + 1];
+
+            for (long i = 0; i < primes.Length; i++)
+            {
+                primes[i] = true;
+            }
+
+            primes[0] = false;
+            primes[1] = false;
+
+            for (long p = 0; p < primes.Length; p++)
+            {
+                if (primes[p] == true)
+                {
+                    Console.Write(p);
+                    if (p < primes.Length)
+                        Console.Write(" ");
+
+                    for (long i = p; i < primes.Length; i++)
+                    {
+                        if (i * p < primes.Length)
+                        {
+                            primes[i * p] = false;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Csharp-Array-Exercises/06.MaxSequenceOfEqualElements.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxSequenceofEqualElements
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().
+                Select(int.Parse).ToArray();
+
+            int start = 0;
+            int count = 0;
+            int max = 0;
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                if (numbers[i] == numbers[i + 1])
+                {
+                    count++;
+                    if (count > max)
+                    {
+                        start = i - count;
+                        max = count;
+                    }
+                }
+                else
+                {
+                    count = 0;
+                }
+            }
+
+            var stop = start + max + 1;
+
+            for (int i = start + 1; i <= stop; i++)
+            {
+                Console.Write(numbers[i]);
+                if (i < stop)
+                    Console.Write(" ");
+            }
+        }
+    }
+}
