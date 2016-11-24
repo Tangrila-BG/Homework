94879a2d92c92173e07481da4492567a0c117cde
Ivo Gyumov <phatomiki@gmail.com>
2016-11-24 14:14:40 +0200

    Add files via upload

---
+++ JavaScript/JS-Advanced/Exercise-Modules/03.-Turtles/app.js
@@ -0,0 +1,37 @@
+/**
+ * Created by tangrila on 04-Nov-16.
+ */
+let turtles = require("./turtles");
+
+result.Turtle = turtles.Turtle;
+result.WaterTurtle = turtles.WaterTurtle;
+result.GalapagosTurtle = turtles.GalapagosTurtle;
+result.EvkodianTurtle = turtles.EvkodianTurtle;
+result.NinjaTurtle = turtles.NinjaTurtle;
+
+/*
+ let testWaterTurtle = new turtles.WaterTurtle("Michelangelo", 18, "male", "Sewer");
+ let testGalapagosTurtle = new turtles.GalapagosTurtle("Raphael", 18, "male");
+ let testEvkodianTurtle = new turtles.EvkodianTurtle("Donatello", 18, "male", 100);
+ let testNinjaTurtle = new turtles.NinjaTurtle("Leonardo", 18, "male", "Blue", "Yamato");
+
+ console.log(testWaterTurtle.toString());
+ // Turtle: Michelangelo
+ // Aged - 18; Gender - male
+ // Currently inhabiting Sewer
+
+ console.log(testGalapagosTurtle.toString());
+ // Turtle: Raphael
+ // Aged - 18; Gender - male
+ // Things, eaten this year:
+
+ console.log(testEvkodianTurtle.toString());
+ // Turtle: Donatello
+ // Aged - 18; Gender - male
+ // Evkodium: 5400
+
+ console.log(testNinjaTurtle.toString());
+ // Turtle: Leonardo
+ // Aged - 18; Gender - male
+ // Leo wears a Blue mask, and is an apprentice with the Yamato.
+*/
