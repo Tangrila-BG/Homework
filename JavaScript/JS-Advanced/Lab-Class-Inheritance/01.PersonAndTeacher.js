7b013402e74f15c707b889dcde2fc08e8032b7ac
Ivo Gyumov <phatomiki@gmail.com>
2016-11-24 14:17:11 +0200

    Add files via upload

---
+++ JavaScript/JS-Advanced/Lab-Class-Inheritance/01.PersonAndTeacher.js
@@ -0,0 +1,22 @@
+/**
+ * Created by tangrila on 02-Nov-16.
+ */
+function personAndTeacher() {
+	//TODO
+	class Person {
+		constructor(name, email) {
+			this.name = name;
+			this.email = email;
+		}
+	}
+	class Teacher extends Person {
+		constructor(name, email, subject) {
+			super(name, email);
+			this.subject = subject;
+		}
+	}
+	return {
+		Person,
+		Teacher
+	}
+}
